b3e08e5cc9b17efbce5eb6151518cf57c616790a
BaikaiL <490051963@qq.com>
2025-12-10 20:49:36 +0800

    改用mybatis-plus；实现新增员工

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -69,6 +70,11 @@ public class EmployeeController {
     @PostMapping("/logout")
     public Result<String> logout() {
         return Result.success();
+    }
+
+    @PostMapping
+    public Result save(EmployeeDTO employeeDTO){
+        return employeeService.save(employeeDTO);
     }
 
 }
--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,18 +1,18 @@
 package com.sky.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
-@Mapper
-public interface EmployeeMapper {
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
-    /**
-     * 根据用户名查询员工
-     * @param username
-     * @return
-     */
-    @Select("select * from employee where username = #{username}")
-    Employee getByUsername(String username);
+@Mapper
+public interface EmployeeMapper extends BaseMapper<Employee> {
 
 }
