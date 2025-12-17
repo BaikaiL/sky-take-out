a9ad6c42a15f63705c2fade5fb9fc15f1c996065
BaikaiL <490051963@qq.com>
2025-12-17 21:34:50 +0800

    定时任务：每分钟处理支付超时订单；每天凌晨1点处理代送达订单

---
+++ sky-server/src/main/java/com/sky/tack/OrderTask.java
@@ -0,0 +1,56 @@
+package com.sky.tack;
+
+import com.sky.entity.Orders;
+import com.sky.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@Slf4j
+public class OrderTask {
+
+	@Autowired
+	private OrderService orderService;
+
+	@Scheduled(cron = "0 * * * * ?")
+	public void processingTimeoutOrders() {
+		log.info("处理支付超时订单开始");
+		LocalDateTime checkTime = LocalDateTime.now().minusMinutes(15);
+		List<Orders> list = orderService.lambdaQuery().eq(Orders::getStatus, Orders.PENDING_PAYMENT)
+				.lt(Orders::getOrderTime, checkTime)
+				.list();
+
+		if (list != null && !list.isEmpty()) {
+			for (Orders orders : list) {
+				orders.setStatus(Orders.CANCELLED);
+				orders.setCancelReason("支付超时");
+				orders.setCancelTime(LocalDateTime.now());
+			}
+			orderService.updateBatchById(list);
+		}
+
+		log.info("处理支付超时订单结束");
+	}
+
+	@Scheduled(cron = "0 0 1 * * ?")
+	private void processDeliveryOrders(){
+		log.info("处理本日未确认订单开始");
+		LocalDateTime checkTime = LocalDateTime.now().minusHours(1);
+		List<Orders> list = orderService.lambdaQuery().eq(Orders::getStatus, Orders.DELIVERY_IN_PROGRESS)
+				.lt(Orders::getDeliveryTime, checkTime)
+				.list();
+		if (list != null && !list.isEmpty()) {
+			for (Orders orders : list) {
+				orders.setStatus(Orders.COMPLETED);
+				orders.setDeliveryTime(LocalDateTime.now());
+			}
+			orderService.updateBatchById(list);
+		}
+	}
+
+}
